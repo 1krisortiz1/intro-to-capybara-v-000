0b9e13e55b042d58b2445c7b65850b1b81e95e77
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-30 18:51:53 +0000

    Automatically backed up by Learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -11,7 +11,6 @@ describe "GET '/' - Greeting Form" do
   it 'has a greeting form with a user_name field' do
     visit '/'
 
-
     expect(page).to have_selector("form")
     expect(page).to have_field(:user_name)
   end
